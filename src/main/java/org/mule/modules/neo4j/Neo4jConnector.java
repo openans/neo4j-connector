261a65e7addf92a0822676a3f014755d2399616b
David Dossot <david@dossot.net>
2013-05-29 16:23:10 -0700

    redesigned connector away from RestCalls, using dynamic service discovery

--- src/main/java/org/mule/modules/neo4j/Neo4jConnector.java
+++ src/main/java/org/mule/modules/neo4j/Neo4jConnector.java
@@ -9,21 +9,38 @@
 package org.mule.modules.neo4j;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.Validate;
+import org.mule.api.ConnectionException;
+import org.mule.api.ConnectionExceptionCode;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
 import org.mule.api.annotations.Configurable;
-import org.mule.api.annotations.Module;
+import org.mule.api.annotations.Connect;
+import org.mule.api.annotations.ConnectionIdentifier;
+import org.mule.api.annotations.Connector;
+import org.mule.api.annotations.Disconnect;
 import org.mule.api.annotations.Processor;
+import org.mule.api.annotations.ValidateConnection;
+import org.mule.api.annotations.param.ConnectionKey;
 import org.mule.api.annotations.param.Default;
 import org.mule.api.annotations.param.Optional;
-import org.mule.api.annotations.rest.HttpMethod;
-import org.mule.api.annotations.rest.RestCall;
-import org.mule.api.annotations.rest.RestExceptionOn;
-import org.mule.api.annotations.rest.RestHeaderParam;
-import org.mule.api.annotations.rest.RestQueryParam;
+import org.mule.api.context.MuleContextAware;
+import org.mule.api.transformer.Transformer;
+import org.mule.endpoint.URIBuilder;
 import org.mule.modules.neo4j.model.CypherQuery;
 import org.mule.modules.neo4j.model.CypherQueryResult;
 import org.mule.modules.neo4j.model.ServiceRoot;
+import org.mule.transformer.types.DataTypeFactory;
+import org.mule.transformer.types.MimeTypes;
+import org.mule.transport.http.HttpConnector;
+import org.mule.transport.http.HttpConstants;
 import org.mule.util.StringUtils;
 
 /**
@@ -35,17 +52,9 @@ import org.mule.util.StringUtils;
  * 
  * @author MuleSoft Inc.
  */
-@Module(name = "neo4j", schemaVersion = "3.4", friendlyName = "Neo4j", minMuleVersion = "3.4.0", description = "Neo4j Module")
-public abstract class Neo4jConnector
+@Connector(name = "neo4j", schemaVersion = "3.4", friendlyName = "Neo4j", minMuleVersion = "3.4.0", description = "Neo4j Module")
+public class Neo4jConnector implements MuleContextAware
 {
-    /**
-     * The base URI of the Neo4j server. It shouldn't end with a trailing slash.
-     */
-    @Configurable
-    @Optional
-    @Default("http://localhost:7474/db/data")
-    private String baseUri;
-
     /**
      * The user used to authenticate to Neo4j.
      */
@@ -63,14 +72,136 @@ public abstract class Neo4jConnector
     /**
      * Should streaming be used when communicating with the Neo4j server.
      */
-    @RestHeaderParam("X-Stream")
     @Configurable
     @Optional
     @Default("true")
     private boolean streaming;
 
-    @RestHeaderParam("Authorization")
+    /**
+     * The connector to use to reach Neo4j: configure only if there is more than one HTTP/HTTPS
+     * connector active in your Mule application.
+     */
+    @Configurable
+    @Optional
+    private org.mule.api.transport.Connector connector;
+
+    private MuleContext muleContext;
     private String authorization;
+    private String baseUri;
+    private ServiceRoot serviceRoot;
+
+    @Connect
+    public void connect(@ConnectionKey @Default("http://localhost:7474/db/data") final String baseUri)
+        throws ConnectionException
+    {
+        try
+        {
+            new URI(baseUri);
+        }
+        catch (final URISyntaxException urie)
+        {
+            throw new ConnectionException(ConnectionExceptionCode.UNKNOWN_HOST, null, "Invalid baseUri: "
+                                                                                      + baseUri, urie);
+        }
+
+        this.baseUri = baseUri;
+
+        try
+        {
+            serviceRoot = getEntityFromApi(baseUri + "/", ServiceRoot.class);
+        }
+        catch (final MuleException me)
+        {
+            throw new ConnectionException(ConnectionExceptionCode.CANNOT_REACH, null,
+                "Failed to retrieve service root from: " + baseUri, me);
+        }
+    }
+
+    @ValidateConnection
+    public boolean isConnected()
+    {
+        return serviceRoot != null;
+    }
+
+    @Disconnect
+    public void disconnect() throws IOException
+    {
+        serviceRoot = null;
+    }
+
+    private <T> T getEntityFromApi(final String uri,
+                                   final Class<T> responseClass,
+                                   final Object... queryParameters) throws MuleException
+    {
+        final MuleMessage response = muleContext.getClient().send(buildUri(uri, queryParameters), null,
+            getRequestProperties(HttpConstants.METHOD_GET));
+
+        return response.getPayload(responseClass);
+    }
+
+    private <T> T postEntityToApi(final String uri,
+                                  final Object entity,
+                                  final Class<T> responseClass,
+                                  final Object... queryParameters) throws MuleException
+    {
+        final Map<String, Object> requestProperties = getRequestProperties(HttpConstants.METHOD_POST);
+
+        requestProperties.put(HttpConstants.HEADER_CONTENT_TYPE, MimeTypes.JSON);
+
+        final Transformer transformer = muleContext.getRegistry().lookupTransformer(
+            DataTypeFactory.create(entity.getClass()), DataTypeFactory.JSON_STRING);
+
+        final Object json = transformer.transform(entity);
+
+        final MuleMessage response = muleContext.getClient().send(buildUri(uri, queryParameters), json,
+            requestProperties);
+
+        return response.getPayload(responseClass);
+    }
+
+    private String buildUri(final String uri, final Object... queryParameters)
+    {
+        Validate.isTrue(queryParameters.length % 2 == 0, "queryParameters must be an even array");
+
+        final URIBuilder uriBuilder = new URIBuilder(uri, muleContext);
+
+        final Map<Object, Object> queryMap = new HashMap<Object, Object>();
+
+        if (connector != null)
+        {
+            queryMap.put("connector", connector.getName());
+        }
+
+        for (int i = 0; i < queryParameters.length; i += 2)
+        {
+            final Object value = queryParameters[i + 1];
+
+            if (value != null)
+            {
+                queryMap.put(queryParameters[i], value.toString());
+            }
+        }
+
+        uriBuilder.setQueryMap(queryMap);
+
+        return uriBuilder.toString();
+    }
+
+    private Map<String, Object> getRequestProperties(final String method)
+    {
+        final Map<String, Object> properties = new HashMap<String, Object>();
+
+        properties.put(HttpConstants.HEADER_ACCEPT, MimeTypes.JSON);
+        properties.put(HttpConnector.HTTP_METHOD_PROPERTY, method);
+        properties.put("X-Stream", streaming);
+
+        if (StringUtils.isNotBlank(authorization))
+        {
+            properties.put(HttpConstants.HEADER_AUTHORIZATION, authorization);
+        }
+
+        return properties;
+    }
 
     /**
      * Get service root.
@@ -81,9 +212,10 @@ public abstract class Neo4jConnector
      * @throws IOException if anything goes wrong with the operation.
      */
     @Processor
-    // FIXME de-hardcode the uri
-    @RestCall(uri = "http://localhost:7474/db/data", method = HttpMethod.GET, contentType = "application/json", exceptions = {@RestExceptionOn(expression = "#[message.inboundProperties['http.status'] != 200]")})
-    public abstract ServiceRoot getServiceRoot() throws IOException;
+    public ServiceRoot getServiceRoot()
+    {
+        return serviceRoot;
+    }
 
     /**
      * Run a cypher query.
@@ -94,19 +226,27 @@ public abstract class Neo4jConnector
      * @param profile defines if a profile of the executed query must be returned
      * @param cypherQuery the query to execute
      * @return a {@link CypherQueryResult}.
+     * @throws MuleException
      * @throws IOException if anything goes wrong with the operation.
      */
     @Processor
-    // FIXME de-hardcode the uri
-    @RestCall(uri = "http://localhost:7474/db/data/cypher", method = HttpMethod.POST, contentType = "application/json", exceptions = {@RestExceptionOn(expression = "#[message.inboundProperties['http.status'] != 200]")})
-    public abstract CypherQueryResult runCypherQuery(@RestQueryParam("includeStats") @Optional @Default("false") boolean includeStatistics,
-                                                     @RestQueryParam("profile") @Optional @Default("false") boolean profile,
-                                                     CypherQuery cypherQuery) throws IOException;
+    public CypherQueryResult runCypherQuery(@Optional @Default("false") final boolean includeStatistics,
+                                            @Optional @Default("false") final boolean profile,
+                                            final CypherQuery cypherQuery) throws MuleException
+    {
+        return postEntityToApi(serviceRoot.getCypher(), cypherQuery, CypherQueryResult.class, "includeStats",
+            includeStatistics, "profile", profile);
+    }
 
     private void refreshAuthorization()
     {
         final byte[] credentialBytes = (StringUtils.trimToEmpty(user) + ":" + StringUtils.trimToEmpty(password)).getBytes();
         authorization = "Basic " + new String(Base64.encodeBase64(credentialBytes));
+    }
+
+    public void setMuleContext(final MuleContext muleContext)
+    {
+        this.muleContext = muleContext;
     }
 
     public String getAuthorization()
@@ -136,14 +276,10 @@ public abstract class Neo4jConnector
         refreshAuthorization();
     }
 
+    @ConnectionIdentifier
     public String getBaseUri()
     {
         return baseUri;
-    }
-
-    public void setBaseUri(final String baseUri)
-    {
-        this.baseUri = baseUri;
     }
 
     // non-JavaBean accessor required by DevKit: http://www.mulesoft.org/jira/browse/DEVKIT-365
@@ -160,5 +296,15 @@ public abstract class Neo4jConnector
     public void setStreaming(final boolean streaming)
     {
         this.streaming = streaming;
+    }
+
+    public org.mule.api.transport.Connector getConnector()
+    {
+        return connector;
+    }
+
+    public void setConnector(final org.mule.api.transport.Connector connector)
+    {
+        this.connector = connector;
     }
 }
